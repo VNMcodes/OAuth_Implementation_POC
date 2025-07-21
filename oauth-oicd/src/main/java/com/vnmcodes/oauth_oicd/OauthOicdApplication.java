99744cae5ade078e46aed813264f1d16552d1fab
VNMcodes <jayant.singh.x7@gmail.com>
2025-07-22 00:11:45 +0530

    Commiting the dependencies required for implementing OAuth 2 in spring project

---
+++ oauth-oicd/src/main/java/com/vnmcodes/oauth_oicd/OauthOicdApplication.java
@@ -0,0 +1,13 @@
+package com.vnmcodes.oauth_oicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthOicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthOicdApplication.class, args);
+	}
+
+}
---
+++ oauth-oicd/src/test/java/com/vnmcodes/oauth_oicd/OauthOicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vnmcodes.oauth_oicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthOicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
